60f2046689831a8add69c08584f76fee852b2b69
Jescara <161954620+Jescara@users.noreply.github.com>
2025-05-20 14:19:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+process.env.LD_LIBRARY_PATH = __dirname + '/instantclient/instantclient_23_8';
+
+const oracledb = require('oracledb');
+oracledb.initOracleClient({ libDir: './instantclient/instantclient_23_8' });
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', async (req, res) => {
+  res.send('Oracle-backed Node.js app is up!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 // 1. Install dependencies: npm install express dotenv cors
 import express, { json }  from 'express';
 import 'dotenv/config';
