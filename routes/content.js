3bfa52b29dbf8242f948cbcd9c6caff28cf9a27d
Jescara <161954620+Jescara@users.noreply.github.com>
2025-05-21 20:45:53 -0400

    Update content.js

--- routes/content.js
+++ routes/content.js
@@ -2,15 +2,26 @@ import { Router } from 'express';
 const router = Router();
 import { getCourseContent } from '../config/mongo.js';
 
-router.get('/course-content/:courseId', async (req, res) => {
-  const { courseId } = req.params;
+router.get('/course-content', async (req, res) => {
   try {
-    const content = await getCourseContent(courseId);
+    const content = await getAllCourseContent(); // returns all docs
     res.json({ status: 'success', data: content });
   } catch (err) {
     console.error(err);
     res.status(500).json({ status: 'error', message: err.message });
   }
 });
+
+
+// router.get('/course-content/:courseId', async (req, res) => {
+//   const { courseId } = req.params;
+//   try {
+//     const content = await getCourseContent(courseId);
+//     res.json({ status: 'success', data: content });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ status: 'error', message: err.message });
+//   }
+// });
 
 export default router;
